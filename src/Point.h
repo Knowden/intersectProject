62fc8211acef1a94719512b1cb01f53a45730399
Nocturne <853172766@qq.com>
2020-03-07 23:31:20 +0800

    define hash and equal function of Point

--- src/Point.h
+++ src/Point.h
@@ -17,4 +17,17 @@ public:
 	double getDistanceToLine(const Line& line);
 };
 
+namespace std {
+    template <>
+    struct hash<Point> {
+        size_t operator()(Point const& p) const noexcept {
+            return ((51 + std::hash<double>()(p.x)) * 51 + std::hash<double>()(p.y));
+        }
+    };
+
+	inline bool operator == (const Point& p1, const Point& p2) {
+		return p1.x == p2.x && p1.y == p2.y;
+	}
+}
+
 #endif // !POINT_H
