b22bac76fd57c44ee1c08762d4fa4a78d9a58efd
Nocturne <853172766@qq.com>
2020-03-07 18:34:35 +0800

    finish Solution class

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -1,1 +1,71 @@
 #include "Solution.h"
+#include "StringUtil.h"
+
+using namespace std;
+
+void Solution::add_component(std::string ori_input) {
+	if (StringUtil::trim(ori_input).at(0) == 'L') {
+		Line line(ori_input);
+		if (line_map.count(line.k) == 0) {
+			line_map[line.k] = *new vector<Line>();
+		}
+		line_map.at(line.k).push_back(line);
+	}
+	else {
+		Circle circle(ori_input);
+		circle_list.push_back(circle);
+	}
+}
+
+int Solution::count_result() {
+	result_set = *new set<Point>();
+	count_line_with_line();
+	count_circle_with_circle();
+	count_line_with_circle();
+	return (int)result_set.size();
+}
+
+void Solution::count_line_with_line() {
+	for (map<double, vector<Line>>::iterator i = line_map.begin(); i != line_map.end(); i++) {
+		map<double, vector<Line>>::iterator temp = i;
+		temp++;
+		for (map<double, vector<Line>>::iterator j = temp; j != line_map.end(); j++) {
+			vector<Line> line_list1 = i->second;
+			vector<Line> line_list2 = j->second;
+
+			for (Line line1 : line_list1) {
+				for (Line line2 : line_list2) {
+					Point* p = line1.get_intersection_with(line2);
+					if (p != nullptr) {
+						result_set.insert(*p);
+					}
+				}
+			}
+		}
+	}
+}
+
+void Solution::count_circle_with_circle() {
+	for (int i = 0; i < (int) circle_list.size(); i++) {
+		for (int j = i + 1; j < (int)circle_list.size(); j++) {
+			vector<Point> v = circle_list.at(i).getIntersectionWith(circle_list.at(j));
+			for (Point point : v) {
+				result_set.insert(point);
+			}
+		}
+	}
+}
+
+void Solution::count_line_with_circle() {
+	for (map<double, vector<Line>>::iterator i = line_map.begin(); i != line_map.end(); i++) {
+		vector<Line> line_list = i->second;
+		for (Line line : line_list) {
+			for (Circle circle : circle_list) {
+				vector<Point> v = circle.getIntersectionWith(line);
+				for (Point point : v) {
+					result_set.insert(point);
+				}
+			}
+		}
+	}
+}
--- src/Solution.h
+++ src/Solution.h
@@ -1,4 +1,28 @@
-#pragma once
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <map>
+#include <vector>
+#include <string>
+#include <set>
+
+#include "Line.h"
+#include "Circle.h"
+#include "Point.h"
+
 class Solution {
+private:
+	std::map<double, std::vector<Line>> line_map;
+	std::vector<Circle> circle_list;
+	std::set<Point> result_set;
+
+	void count_line_with_line();
+	void count_circle_with_circle();
+	void count_line_with_circle();
+
+public:
+	void add_component(std::string ori_input);
+	int count_result();
 };
 
+#endif
